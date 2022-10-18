ac28c40765b6c52166dff16dd430cf0465e62b8f
Ekta kumari <kumariekta1430@gmail.com>
2022-10-18 19:29:18 +0530

    first non-repeating character in string

--- queues/firstNonRepeatingChar.js
+++ queues/firstNonRepeatingChar.js
@@ -1,39 +1,31 @@
 function firstNonRepeating(str) {
-    // count array of size 26(assuming
-    // only lower case characters are present)
-    var charCount = new Array(MAX_CHAR).fill(0);
+    let charCount = new Array(26).fill(0);
+    let q = [];
 
-    // Queue to store Characters
-    var q = [];
 
-    // traverse whole stream
-    for (var i = 0; i < str.length; i++) {
-      var ch = str[i];
+    for (let i = 0; i < str.length; i++) {
+      let ch = str[i];
 
-      // push each character in queue
       q.push(ch);
 
-      // increment the frequency count
-      charCount[ch.charCodeAt(0) - "a".charCodeAt(0)]++;
 
-      // check for the non repeating character
+      charCount[ch.charCodeAt(0) - 'a'.charCodeAt(0)]++;
       while (q.length > 0) {
         if (charCount[q[0].charCodeAt(0) -
         "a".charCodeAt(0)] > 1) {
           q.shift();
         } else {
-          document.write(q[0] + " ");
+          console.log(q[0] + " ");
           break;
         }
       }
       if (q.length == 0) {
-        document.write(-1 + " ");
+       console.log(-1 + " ");
       }
     }
-    document.write("<br>");
   }
 
-  // Driver function
-  var str = "aabc";
+
+  let str = "abab";
   firstNonRepeating(str);
    
